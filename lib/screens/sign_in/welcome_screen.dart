b3d73edc636d95944dbcccbb553a565e4c71d48d
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-05-30 20:26:33 +0700

    add text and button

--- lib/screens/sign_in/welcome_screen.dart
+++ lib/screens/sign_in/welcome_screen.dart
@@ -7,7 +7,56 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Backgroud(
-      child: Container(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Build Your Next\nFuture Career Like\na Master',
+              style: textLogoNormal,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              '18.000 Jobs Available',
+              style: body.copyWith(color: white),
+            ),
+            Spacer(),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Get Started',
+                        style: body.copyWith(color: primaryColor),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          primary: white,
+                          shape: StadiumBorder(),
+                          minimumSize: Size(200, 45)),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Sign In'),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.transparent,
+                          shape: StadiumBorder(),
+                          shadowColor: Colors.transparent,
+                          side: BorderSide(width: 2, color: primaryColor),
+                          minimumSize: Size(200, 45)),
+                    ),
+                  ],
+                )),
+          ],
+        ),
+      ),
     ));
   }
 }
