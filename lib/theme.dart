0644db91cf9c7b09fb59c687051e521c29213725
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-05-30 19:06:35 +0700

    first commit

---
+++ lib/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+// NOTE: Color
+Color primaryColor = Color(0xff4141A4);
+Color white = Colors.white;
+Color black = Color(0xff272C2F);
+Color greyText = Color(0xffB3B5C4);
+Color grey = Color(0xffF1F0F5);
+Color darkgrey = Color(0xffECEDF1);
+Color red = Color(0xffFD4F56);
+
+// NOTE: TextStyle
+TextStyle textLogoBold =
+    TextStyle(color: white, fontWeight: FontWeight.w600, fontSize: 32);
+TextStyle textLogoNormal =
+    TextStyle(color: white, fontWeight: FontWeight.w400, fontSize: 32);
+TextStyle h1 =
+    TextStyle(color: black, fontWeight: FontWeight.w600, fontSize: 24);
+TextStyle h2 =
+    TextStyle(color: black, fontWeight: FontWeight.w600, fontSize: 20);
+TextStyle body =
+    TextStyle(color: black, fontWeight: FontWeight.w500, fontSize: 14);
+TextStyle bodyLight =
+    TextStyle(color: black, fontWeight: FontWeight.w300, fontSize: 14);
+TextStyle subtitle =
+    TextStyle(color: greyText, fontWeight: FontWeight.w400, fontSize: 14);
+TextStyle subtitleLight =
+    TextStyle(color: greyText, fontWeight: FontWeight.w300, fontSize: 14);
