b16eb345f34f819a58d2c6a003714d8241767bd5
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-05-30 20:06:23 +0700

    create background

---
+++ lib/screens/sign_in/welcome_screen.dart
@@ -0,0 +1,13 @@
+import 'package:bwa_job/screens/sign_in/components/background.dart';
+import 'package:bwa_job/theme.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Backgroud(
+      child: Container(),
+    ));
+  }
+}
