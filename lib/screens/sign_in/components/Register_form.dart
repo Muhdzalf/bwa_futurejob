3d2d1fcf991ef38f9d34836fd554adf8049cbf68
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-05-31 20:29:15 +0700

    add text field

--- lib/screens/sign_in/components/Register_form.dart
+++ lib/screens/sign_in/components/Register_form.dart
@@ -1,3 +1,4 @@
+import 'package:bwa_job/screens/sign_in/components/textfield_label.dart';
 import 'package:bwa_job/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -6,44 +7,64 @@ class RegisterForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(left: 24, top: 30),
-              child: Column(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 24, top: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Sign Up',
+                      style: subtitle,
+                    ),
+                    SizedBox(
+                      height: 2,
+                    ),
+                    Text(
+                      'Begin New Journey',
+                      style: h1,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 50),
+                padding: EdgeInsets.all(4),
+                height: 120,
+                width: 120,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      width: 2,
+                      color: primaryColor,
+                    )),
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/profile.png'),
+                ),
+              ),
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    'Sign Up',
-                    style: subtitle,
+                  TextFieldWithLabel(
+                    label: 'Full Name',
                   ),
-                  SizedBox(
-                    height: 2,
+                  TextFieldWithLabel(
+                    label: 'Email Address',
                   ),
-                  Text(
-                    'Begin New Journey',
-                    style: h1,
+                  TextFieldWithLabel(
+                    label: 'Password',
+                  ),
+                  TextFieldWithLabel(
+                    label: 'Your Goal',
                   ),
                 ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 50),
-              padding: EdgeInsets.all(4),
-              height: 120,
-              width: 120,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    width: 2,
-                    color: primaryColor,
-                  )),
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/profile.png'),
-              ),
-            ),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
