5c05089d9958153f4b7fc289c74636e21df558b0
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-05-31 20:17:09 +0700

    add text and photo

--- lib/screens/sign_in/components/Register_form.dart
+++ lib/screens/sign_in/components/Register_form.dart
@@ -1,11 +1,50 @@
+import 'package:bwa_job/theme.dart';
 import 'package:flutter/material.dart';
 
 class RegisterForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 24, top: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Sign Up',
+                    style: subtitle,
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    'Begin New Journey',
+                    style: h1,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 50),
+              padding: EdgeInsets.all(4),
+              height: 120,
+              width: 120,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    width: 2,
+                    color: primaryColor,
+                  )),
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/profile.png'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
