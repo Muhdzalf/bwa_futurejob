1f5954e68aaf5720882a8e4d21bbe8ec0dbe5eda
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-06-02 21:03:31 +0700

    add Categories List

---
+++ lib/screens/home/components/category_card.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../../../theme.dart';
+
+class CategoryCard extends StatelessWidget {
+  final String title;
+  final String image;
+
+  const CategoryCard({
+    Key key,
+    this.title,
+    this.image,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: Container(
+          width: 150,
+          decoration: BoxDecoration(
+              image:
+                  DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.transparent, primaryColor.withOpacity(0.7)],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter)),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  title,
+                  style: body.copyWith(color: white, fontSize: 18),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:bwa_job/theme.dart';
 import 'package:flutter/material.dart';
 
+import 'components/category_card.dart';
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,7 +12,7 @@ class HomeScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Padding(
-              padding: EdgeInsets.only(left: 24, top: 30, right: 24),
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 30),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -40,10 +42,50 @@ class HomeScreen extends StatelessWidget {
                     child: CircleAvatar(
                       backgroundImage: AssetImage('assets/images/profile.png'),
                     ),
-                  )
+                  ),
                 ],
               ),
             ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 24, bottom: 16),
+                  child: Text(
+                    'Hot Categories',
+                    style: body,
+                  ),
+                ),
+                SizedBox(
+                  height: 200,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      CategoryCard(
+                        title: 'Website Developer',
+                        image: 'assets/images/background/website.png',
+                      ),
+                      CategoryCard(
+                        title: 'Mobile Developer',
+                        image: 'assets/images/background/mobile.png',
+                      ),
+                      CategoryCard(
+                        title: 'Product Designer',
+                        image: 'assets/images/background/designer.png',
+                      ),
+                      CategoryCard(
+                        title: 'Video\nEditor',
+                        image: 'assets/images/background/video.png',
+                      ),
+                      CategoryCard(
+                        title: 'Content Writer',
+                        image: 'assets/images/background/writer.png',
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
