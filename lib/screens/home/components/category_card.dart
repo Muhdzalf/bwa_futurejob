0df14bf72961a00b5080ef3773d97a152ad24a16
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-06-08 18:43:12 +0700

    add navigator route to detail screen

--- lib/screens/home/components/category_card.dart
+++ lib/screens/home/components/category_card.dart
@@ -1,3 +1,4 @@
+import 'package:bwa_job/screens/home/details/details_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../../../theme.dart';
@@ -18,24 +19,30 @@ class CategoryCard extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 8),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(16),
-        child: Container(
-          width: 150,
-          decoration: BoxDecoration(
-              image:
-                  DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
+        child: InkWell(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => DetailScreen()));
+          },
           child: Container(
+            width: 150,
             decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [Colors.transparent, primaryColor.withOpacity(0.7)],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter)),
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  title,
-                  style: body.copyWith(color: white, fontSize: 18),
+                image: DecorationImage(
+                    image: AssetImage(image), fit: BoxFit.cover)),
+            child: Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                Colors.transparent,
+                primaryColor.withOpacity(0.7)
+              ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    title,
+                    style: body.copyWith(color: white, fontSize: 18),
+                  ),
                 ),
               ),
             ),
