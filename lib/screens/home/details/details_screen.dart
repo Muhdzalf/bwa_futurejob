085844667aab900f74a788de34a790ded53c6f2d
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-06-16 15:24:38 +0700

    add logo and job title

--- lib/screens/home/details/details_screen.dart
+++ lib/screens/home/details/details_screen.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
 
+import '../../../theme.dart';
+
 class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: 80,
+          ),
+          Container(
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/images/social_media/google.png',
+                  height: 60,
+                ),
+                Text(
+                  'Front-End Developer',
+                  style: h1,
+                ),
+                Text(
+                  'Google inc - Jakarta',
+                  style: subtitle,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 32,
+          ),
+          Container()
+        ],
+      ),
+    );
   }
 }
