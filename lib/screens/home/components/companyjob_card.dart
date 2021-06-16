83a698287bc9cd3a0028bd241dc7397de58ff69e
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-06-16 15:09:35 +0700

    add page route in job card to details screen

--- lib/screens/home/components/companyjob_card.dart
+++ lib/screens/home/components/companyjob_card.dart
@@ -1,3 +1,4 @@
+import 'package:bwa_job/screens/home/details/details_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../../../theme.dart';
@@ -15,42 +16,48 @@ class CompanyJobCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Image.asset(
-              imageUrl,
-            ),
-          ),
-          SizedBox(
-            width: 27,
-          ),
-          Expanded(
-            flex: 5,
-            child: Container(
-              decoration: BoxDecoration(
-                  border:
-                      Border(bottom: BorderSide(width: 1, color: greyText))),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    job,
-                    style: body.copyWith(fontSize: 18),
-                  ),
-                  Text(
-                    companyName,
-                    style: subtitleLight,
-                  ),
-                  SizedBox(height: 18)
-                ],
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context, (MaterialPageRoute(builder: (context) => DetailScreen())));
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Image.asset(
+                imageUrl,
               ),
             ),
-          )
-        ],
+            SizedBox(
+              width: 27,
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(
+                decoration: BoxDecoration(
+                    border:
+                        Border(bottom: BorderSide(width: 1, color: greyText))),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      job,
+                      style: body.copyWith(fontSize: 18),
+                    ),
+                    Text(
+                      companyName,
+                      style: subtitleLight,
+                    ),
+                    SizedBox(height: 18)
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
