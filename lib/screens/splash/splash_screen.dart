0130b91904cb2e0233699d278f0239c78df3a425
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-06-21 16:47:04 +0700

    change time to 3 second

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -14,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    Timer(Duration(milliseconds: 500), () {
+    Timer(Duration(seconds: 3), () {
       Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => WelcomeScreen()));
     });
